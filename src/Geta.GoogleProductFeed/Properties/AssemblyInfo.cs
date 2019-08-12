0d22cb27f1723fab955aea8f70c6fa300bf92785
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2019-08-12 06:54:43 +0300

    Added package icon

--- src/Geta.GoogleProductFeed/Properties/AssemblyInfo.cs
+++ src/Geta.GoogleProductFeed/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Geta.GoogleProductFeed")]
 [assembly: AssemblyDescription("Google Product Feed")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Geta")]
 [assembly: AssemblyProduct("Geta.GoogleProductFeed")]
-[assembly: AssemblyCopyright("Copyright © Geta 2016")]
+[assembly: AssemblyCopyright("Copyright © Geta 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b1ebefd4-ec96-4184-8e83-a4989dab73cd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
