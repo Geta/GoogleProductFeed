150fc4716ef68036a308f6b7a9323a3eb0f50ea3
Zbigniew Winiarski <zbigniew.winiarski@getadigital.com>
2021-05-28 07:36:47 +0200

    Add more fields to Entry model

---
+++ src/Geta.GoogleProductFeed/Models/SubscriptionCost.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace Geta.GoogleProductFeed.Models
+{
+    public class SubscriptionCost
+    {
+        [XmlElement("period", Namespace = "http://base.google.com/ns/1.0")]
+        public string Period { get; set; }
+        
+        [XmlElement("period_length", Namespace = "http://base.google.com/ns/1.0")]
+        public int PeriodLength { get; set; }
+        
+        [XmlElement("amount", Namespace = "http://base.google.com/ns/1.0")]
+        public string Amount { get; set; }
+    }
+}
---
+++ src/Geta.GoogleProductFeed/Models/Tax.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace Geta.GoogleProductFeed.Models
+{
+    public class Tax
+    {
+        [XmlElement("country", Namespace = "http://base.google.com/ns/1.0")]
+        public string Country { get; set; }
+        
+        [XmlElement("region", Namespace = "http://base.google.com/ns/1.0")]
+        public string Region { get; set; }
+        
+        [XmlElement("rate", Namespace = "http://base.google.com/ns/1.0")]
+        public double Rate { get; set; }
+        
+        [XmlElement("tax_ship", Namespace = "http://base.google.com/ns/1.0")]
+        public string TaxShip { get; set; }
+    }
+}
